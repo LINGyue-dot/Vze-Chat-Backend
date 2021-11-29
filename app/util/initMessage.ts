ceeefa58753aee41759aae1ef686d689e741bd67
Sean Chen <qianlonwork@outlook.com>
2021-11-29 19:51:28 +0800

    feat: 🎸 添加部分引导性信息文案

--- app/util/initMessage.ts
+++ app/util/initMessage.ts
@@ -2,7 +2,7 @@
  * @Author: qianlong github:https://github.com/LINGyue-dot
  * @Date: 2021-11-29 11:04:38
  * @LastEditors: qianlong github:https://github.com/LINGyue-dot
- * @LastEditTime: 2021-11-29 11:39:11
+ * @LastEditTime: 2021-11-29 19:50:09
  * @Description:
  */
 
@@ -20,7 +20,7 @@ module.exports = {
       from_user_id: "1",
       to_user_id: user_id,
       send_time: Date.now(),
-      message: `<p>hi</p><a-image class="input-img" src="http://images.qianlon.cn:5700/ef2e439b8e7eedd50aba92da9150672a.png"/><p><br/></p><a-image class="input-img" src="http://images.qianlon.cn:5700/e00bfbdcc9a49500b68259627f7c4ef0.png"/>`,
+      message: `<p>hi</p><p>Github 链接：https://github.com/LINGyue-dot/Vze-Chat-Frontend</p><a-image class="input-img" src="http://images.qianlon.cn:5700/ef2e439b8e7eedd50aba92da9150672a.png"/><p><br/></p><a-image class="input-img" src="http://images.qianlon.cn:5700/e00bfbdcc9a49500b68259627f7c4ef0.png"/>`,
     });
   },
 };
